28e4d493022321c74cc5b6cacae1ece2eadc8819
Gopal Punvantwar <gopalpunvantwar@gmail.com>
2020-10-22 22:58:44 +0530

    Spring boot project is created without adding any extra dependency.

---
+++ src/main/java/in/talentbox/boxapp/BoxAppApplication.java
@@ -0,0 +1,13 @@
+package in.talentbox.boxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/talentbox/boxapp/BoxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.talentbox.boxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
